950cbb42b5bfc2f338fbcc9324f446a14bb6a21c
MohammadAli Sedaghat <mohammadali.sedaghat.2019@gmail.com>
2021-10-31 20:58:16 +0330

    add app listen

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
 
@@ -18,3 +19,8 @@ mongoose
   .then(() => {
     console.log("DB connection successful!");
   });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});

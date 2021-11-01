0a7770a0e2b5147d70a389e3bdd9321a2886115e
Ali Golestan <alipishbiinn7878@gmail.com>
2021-11-01 21:28:59 +0330

    added stock model

---
+++ models/stockModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const stockSchema = new mongoose.Schema({
+  st_name: {
+    type: "string",
+    required: [true, "A stock must have a name!"],
+    minLength: [5, "A stock name must have less or equal 5 characters"],
+    maxlength: [255, "A stock name must have more or equal 255 characters"],
+  },
+  st_phone: {
+    type: "string",
+    required: [true, "A stock must have a phone number!"],
+    minLength: [11, "A stock phone number must have equal 11 numbers"],
+    maxlength: [11, "A stock phone number must have equal 11 numbers"],
+  },
+  st_capacity: {
+    type: Number,
+    required: [true, "A stock must have a capacity!"],
+  },
+});

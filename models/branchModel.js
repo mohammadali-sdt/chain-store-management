55044f8301022aeeedbaeb5c927cffd98ab6efc4
Ali Golestan <alipishbiinn7878@gmail.com>
2021-11-01 22:15:09 +0330

    added new model and fix bugs

--- models/branchModel.js
+++ models/branchModel.js
@@ -5,18 +5,18 @@ const branchSchema = new mongoose.Schema({
     type: String,
     minLength: [5, "A branch name must have more or equal 5 characters"],
     maxLength: [255, "A branch name must have less or equal 255 characters"],
-    require: [true, "A branch must have a name"],
+    required: [true, "A branch must have a name"],
   },
   br_owner: {
     type: String,
     minLength: [5, "A branch owner must have more or equal 5 characters"],
     maxLength: [40, "A branch owner must have less or equal 40 characters"],
-    require: [true, "A branch must have a owner"],
+    required: [true, "A branch must have a owner"],
   },
   br_phone: {
     type: String,
     minLength: [11, "A phone must have 11 digit"],
     maxLength: [11, "A phone must have 11 digit"],
-    require: [true, "A branch must have a phone"],
+    required: [true, "A branch must have a phone"],
   },
 });

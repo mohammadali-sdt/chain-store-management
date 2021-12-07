3da239ddb150c69b9025f522e4db7a1b8484680d
MohammadAli Sedaghat <tinivini2017@gmail.com>
2021-12-07 21:09:31 +0330

    add create products

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,3 +5,6 @@ const genericCrud = require('./genericCrud');
 exports.getAllProducts = genericCrud.getAll(productModel.Product);
 exports.createStationery = genericCrud.createOne(productModel.Stationery);
 exports.createClothing = genericCrud.createOne(productModel.Clothing);
+exports.createFoodstuff = genericCrud.createOne(productModel.Foodstuff);
+exports.createHomeAppliance = genericCrud.createOne(productModel.HomeAppliance);
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router()
 router.route('/').get(productController.getAllProducts);
 router.route('/stationery').post(productController.createStationery);
 router.route('/clothing').post(productController.createClothing);
+router.route('/foodstuff').post(productController.createFoodstuff);
+router.route('/homeappliance').post(productController.createHomeAppliance);
 
 module.exports = router;

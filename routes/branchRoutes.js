0452a3e36741d2d190e77bbbc7fce32e19010147
Ali Golestan <alipishbiinn7878@gmail.com>
2021-12-08 21:14:48 +0330

    Add get branches by city query

--- routes/branchRoutes.js
+++ routes/branchRoutes.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const branchController = require('../controllers/branchController');
-
-
+const express = require("express");
+const branchController = require("../controllers/branchController");
 
 const router = express.Router();
 
-router.route('/').get(branchController.getAllBranches).post(branchController.createBranch)
+router
+  .route("/")
+  .get(branchController.getAllBranches)
+  .post(branchController.createBranch);
+
+router.route("/:city").get(branchController.getBranchsByCity);
 
 module.exports = router;

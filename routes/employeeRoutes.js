076c96bf2f97b9e284fa98ef1806b58da15ad681
Ali Golestan <alipishbiinn7878@gmail.com>
2021-12-09 21:24:27 +0330

    add query get employees by city

--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -7,6 +7,6 @@ router.route("/").get(employeeController.getAllEmployee);
 router.route("/stock").post(employeeController.createStockEmployee);
 router.route("/branch").post(employeeController.createBranchEmployee);
 router.route("/central").post(employeeController.createCentralEmployee);
-router.route('/young').get(employeeController.getEmployeesWithAge);
-
+router.route("/young").get(employeeController.getEmployeesWithAge);
+router.route("/:city").get(employeeController.getEmploeeysByCity);
 module.exports = router;

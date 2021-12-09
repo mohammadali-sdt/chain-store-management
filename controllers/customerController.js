892a95fff8c0e09e738ac03ac65ac181c73e1b13
Ali Golestan <alipishbiinn7878@gmail.com>
2021-12-09 20:41:58 +0330

    Add query get customers less than 20

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,5 +1,24 @@
-const Customer = require('../models/customerModel');
-const genericCrud = require('./genericCrud');
+const Customer = require("../models/customerModel");
+const genericCrud = require("./genericCrud");
+const catchAsync = require("../utils/catchAsync");
 
 exports.getAllCustomers = genericCrud.getAll(Customer);
 exports.createCustomer = genericCrud.createOne(Customer);
+
+exports.getCustomersByAge = catchAsync(async (req, res, next) => {
+  const customers = await Customer.aggregate([
+    {
+      $match: { cu_age: { $lt: 20 } },
+    },
+    {
+      $group: { _id: null, count: { $sum: 1 } },
+    },
+  ]);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: customers,
+    },
+  });
+});

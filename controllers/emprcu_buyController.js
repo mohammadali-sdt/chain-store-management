da1138e92c41574f25e57fa7bfa0cb52a4f6a641
MohammadAli Sedaghat <tinivini2017@gmail.com>
2021-12-21 13:43:05 +0330

    add query get 5 best-selling products

---
+++ controllers/emprcu_buyController.js
@@ -0,0 +1,45 @@
+const EmPrCu_Buy = require('../models/emprcu_buy');
+const genericCrud = require("./genericCrud");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllSales = genericCrud.getAll(EmPrCu_Buy);
+
+exports.createSale = genericCrud.createOne(EmPrCu_Buy);
+
+exports.getFiveBestSellingProduct = catchAsync(async (req, res, next) => {
+
+    const data = await EmPrCu_Buy.aggregate([
+        {
+            $unwind: "$pr_list"
+        },
+        {
+            $group:{
+                _id:"$pr_list.pr_id",
+                numOfSelling: { $sum: "$pr_list.number" },
+            }
+        },
+        {
+            $sort: {
+                numOfSelling: -1,
+            }
+        },
+        {
+            $limit: 5
+        },
+        {
+            $lookup:{
+                from:'products',
+                localField: '_id',
+                foreignField: '_id',
+                as: 'product'
+            }
+        }
+    ]);
+
+    res.status(200).json({
+        status: "success",
+        data:{
+            data
+        }
+    })
+});

c356021adaf1345b98583f28a644b57c1ea685f1
Ali Golestan <alipishbiinn7878@gmail.com>
2022-01-09 21:14:44 +0330

    add password hash for employee

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -1,89 +1,90 @@
 const mongoose = require("mongoose");
 const addressSchema = require("./addressSchema");
+const bcrypt = require("bcrypt");
 
 const employeeSchema = new mongoose.Schema(
-    {
-        em_name: {
-            type: String,
-            minLength: [3, "A employee name must have more or equal 3 characters"],
-            maxLength: [40, "A employee name must have less or equal 40 characters"],
-            required: [true, "A employee must have name"],
-        },
-        em_lastname: {
-            type: String,
-            minLength: [
-                3,
-                "A employee lastname must have more or equal 3 characters",
-            ],
-            maxLength: [
-                40,
-                "A employee lastname must have less or equal 40 characters",
-            ],
-            required: [true, "A employee must have lastname"],
-        },
-        em_fatherName: {
-            type: String,
-            minLength: [3, "A name must have more or equal 3 characters"],
-            maxLength: [40, "A name must have less or equal 40 characters"],
-            required: [true, "A employee must have father's Name"],
-        },
-        em_phone: {
-            type: String,
-            minLength: [11, "A phone must have 11 digits"],
-            maxLength: [11, "A phone must have 11 digits"],
-            required: [true, "A employee must have a phone"],
-        },
-        em_id: {
-            type: String,
-            minLength: [10, "A id must have 10 digits"],
-            maxLength: [10, "A id must have 10 digits"],
-            required: [true, "A employee must have a id"],
-            unique: true,
-        },
-        em_naid: {
-            type: String,
-            minLength: [1, "A id must have 1 digits"],
-            maxLength: [10, "A phone must have 10 digits"],
-            required: [true, "A employee must have a id"],
-        },
-        em_brithDate: {
-            type: Date,
-            required: [true, "A employee must have brith date"],
-        },
-        em_personnelCode: {
-            type: String,
-            minLength: [9, "A personnel code must have 9 digits"],
-            maxLength: [9, "A personnel code must have 9 digits"],
-            unique: true,
-            required: [true, "A employee must have a personnel code"],
-        },
-        em_password: {
-            type: String,
-            required: [true, "Please provide a password"],
-            minlength: 8,
-            select: false,
-        },
-        em_passwordConfirm: {
-            type: String,
-            required: [true, "Please confirm your password"],
-            validate: {
-                validator: function (field) {
-                    return field === this.em_password;
-                },
-                message: "Passwords are not the same!",
-            },
-        },
-        em_address: {
-            type: addressSchema,
-            required: [true, "A employee must have address"],
-        },
-        em_age: Number,
+  {
+    em_name: {
+      type: String,
+      minLength: [3, "A employee name must have more or equal 3 characters"],
+      maxLength: [40, "A employee name must have less or equal 40 characters"],
+      required: [true, "A employee must have name"],
     },
-    {
-        toJSON: {virtuals: true},
-        toObject: {virtuals: true},
-        discriminatorKey: "em_role",
-    }
+    em_lastname: {
+      type: String,
+      minLength: [
+        3,
+        "A employee lastname must have more or equal 3 characters",
+      ],
+      maxLength: [
+        40,
+        "A employee lastname must have less or equal 40 characters",
+      ],
+      required: [true, "A employee must have lastname"],
+    },
+    em_fatherName: {
+      type: String,
+      minLength: [3, "A name must have more or equal 3 characters"],
+      maxLength: [40, "A name must have less or equal 40 characters"],
+      required: [true, "A employee must have father's Name"],
+    },
+    em_phone: {
+      type: String,
+      minLength: [11, "A phone must have 11 digits"],
+      maxLength: [11, "A phone must have 11 digits"],
+      required: [true, "A employee must have a phone"],
+    },
+    em_id: {
+      type: String,
+      minLength: [10, "A id must have 10 digits"],
+      maxLength: [10, "A id must have 10 digits"],
+      required: [true, "A employee must have a id"],
+      unique: true,
+    },
+    em_naid: {
+      type: String,
+      minLength: [1, "A id must have 1 digits"],
+      maxLength: [10, "A phone must have 10 digits"],
+      required: [true, "A employee must have a id"],
+    },
+    em_brithDate: {
+      type: Date,
+      required: [true, "A employee must have brith date"],
+    },
+    em_personnelCode: {
+      type: String,
+      minLength: [9, "A personnel code must have 9 digits"],
+      maxLength: [9, "A personnel code must have 9 digits"],
+      unique: true,
+      required: [true, "A employee must have a personnel code"],
+    },
+    em_password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: 8,
+      select: false,
+    },
+    em_passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+      validate: {
+        validator: function (field) {
+          return field === this.em_password;
+        },
+        message: "Passwords are not the same!",
+      },
+    },
+    em_address: {
+      type: addressSchema,
+      required: [true, "A employee must have address"],
+    },
+    em_age: Number,
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    discriminatorKey: "em_role",
+  }
 );
 
 // employeeSchema.virtual("age").get(function () {
@@ -91,10 +92,20 @@ const employeeSchema = new mongoose.Schema(
 //   return current_year - this.em_brithDate.getFullYear();
 // });
 
-employeeSchema.pre('save', function (next) {
-    const current_year = new Date().getFullYear();
-    this.em_age = current_year - this.em_brithDate.getFullYear();
-    next();
+employeeSchema.pre("save", async function (next) {
+  if (!this.isModified("em_password")) {
+    return next();
+  }
+  this.em_password = await bcrypt.hash(this.em_password, 12);
+  this.em_passwordConfirm = undefined;
+
+  next();
+});
+
+employeeSchema.pre("save", function (next) {
+  const current_year = new Date().getFullYear();
+  this.em_age = current_year - this.em_brithDate.getFullYear();
+  next();
 });
 
 // em_role: {
@@ -109,54 +120,54 @@ employeeSchema.pre('save', function (next) {
 const Employee = mongoose.model("Employee", employeeSchema);
 
 const branchEmployeeSchema = new mongoose.Schema({
-    brem_work: {
-        type: mongoose.Types.ObjectId,
-        ref: "Branch",
-        required: [true, "A branch employee must have id of branch"],
-    },
-    brem_employmentDate: {
-        type: Date,
-        default: Date.now(),
-    },
+  brem_work: {
+    type: mongoose.Types.ObjectId,
+    ref: "Branch",
+    required: [true, "A branch employee must have id of branch"],
+  },
+  brem_employmentDate: {
+    type: Date,
+    default: Date.now(),
+  },
 });
 
 const BranchEmployee = Employee.discriminator(
-    "BranchEmployee",
-    branchEmployeeSchema,
-    "branch"
+  "BranchEmployee",
+  branchEmployeeSchema,
+  "branch"
 );
 
 const stockEmployeeSchema = new mongoose.Schema({
-    stem_work: {
-        type: mongoose.Types.ObjectId,
-        ref: "Stock",
-        required: [true, "A stock employee must have id of stock"],
-    },
-    stem_employmentDate: {
-        type: Date,
-        default: Date.now(),
-    },
-    stem_section: {
-        type: String,
-        enum: ["storage", "publish"],
-        required: [true, "stock employee must have a section"],
-    },
-    stem_manager: {
-        type: Boolean,
-        default: false,
-    },
+  stem_work: {
+    type: mongoose.Types.ObjectId,
+    ref: "Stock",
+    required: [true, "A stock employee must have id of stock"],
+  },
+  stem_employmentDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  stem_section: {
+    type: String,
+    enum: ["storage", "publish"],
+    required: [true, "stock employee must have a section"],
+  },
+  stem_manager: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const StockEmployee = Employee.discriminator(
-    "StockEmployee",
-    stockEmployeeSchema,
-    "stock"
+  "StockEmployee",
+  stockEmployeeSchema,
+  "stock"
 );
 const centralEmployeeSchema = new mongoose.Schema({});
 const CentralEmployee = Employee.discriminator(
-    "CentralEmployee",
-    centralEmployeeSchema,
-    "central"
+  "CentralEmployee",
+  centralEmployeeSchema,
+  "central"
 );
 
 exports.Employee = Employee;

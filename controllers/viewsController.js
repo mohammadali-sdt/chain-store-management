b1add9c85dca6a949e97ea25d5adbc0fec481357
MohammadAli Sedaghat <tinivini2017@gmail.com>
2022-01-29 12:16:17 +0330

    add employee page and styles

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,20 @@
 const productModel = require("../models/productModel");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
+const employeeModel = require("../models/employeeModel");
 
 exports.getHomePage = catchAsync(async (req, res, next) => {
   const products = await productModel.Product.find();
-  res.render("home", {
+  res.status(200).render("home", {
     title: "Home",
     products,
+  });
+});
+
+exports.showEmployees = catchAsync(async (req, res, next) => {
+  const employees = await employeeModel.Employee.find();
+  res.status(200).render("employee", {
+    title: "Employees",
+    employees,
   });
 });
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -9,10 +9,12 @@ router.route("/branch").post(employeeController.createBranchEmployee);
 router.route("/central").post(employeeController.createCentralEmployee);
 router.route("/young").get(employeeController.getEmployeesWithAge);
 router.route("/:city").get(employeeController.getEmploeeysByCity);
-router.route('/branch/num').get(employeeController.getNumOfBranchEm);
-router.route('/central/num').get(employeeController.getNumOfCentralEm);
-router.route('/stock/num').get(employeeController.getNumOfStockEm);
-router.route('/stock/managers/:city').get(employeeController.getManagerOfStockByCity);
-router.route('/stock/max').get(employeeController.getStockWithMaxEmployee);
-router.route('/branch/min').get(employeeController.getBranchWithMinEmployee);
+router.route("/branch/num").get(employeeController.getNumOfBranchEm);
+router.route("/central/num").get(employeeController.getNumOfCentralEm);
+router.route("/stock/num").get(employeeController.getNumOfStockEm);
+router
+  .route("/stock/managers/:city")
+  .get(employeeController.getManagerOfStockByCity);
+router.route("/stock/max").get(employeeController.getStockWithMaxEmployee);
+router.route("/branch/min").get(employeeController.getBranchWithMinEmployee);
 module.exports = router;
